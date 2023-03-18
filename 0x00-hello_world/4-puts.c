27cc1b205bfb0f06face78bcf2508b8867aca5bf
Abtechguru <careernig24@gmail.com>
2023-03-18 15:53:36 -0700

    0x00-hello_world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main
+ *main - A progam that print a line with puts function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle);
+return (0);
+}
