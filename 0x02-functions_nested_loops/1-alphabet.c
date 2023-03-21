e6d8faad2283ab78c6fd6a12160d9bdca76eef44
Abtechguru <careernig24@gmail.com>
2023-03-21 13:35:40 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main_h"
+
+/**
+ * print_alphabet - print all alphabet in lowecase
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
